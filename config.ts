4be9a4856ed1cce3a063a6befee1e36fa00ea2e6
Fede_B <83786463+fedeb123@users.noreply.github.com>
2025-05-13 16:30:43 -0300

    Config converted to Singleton Class and added base url

--- config.ts
+++ config.ts
@@ -2,12 +2,18 @@ import dotenv from "dotenv"
 
 dotenv.config({ path: '.env' });
 
-interface config {
-    webPort: number,
-    wppwppAPIToken: string,
-    ownNumberID: string
+interface configType {
+    webPort?: number,
+    wppAPIToken?: string,
+    ownNumberID?: string,
+    baseUrl: string
 };
 
-export const webPort = process.env.PORT;
-export const wppAPIToken = process.env.WHATSAPP_TOKEN;
-export const ownNumberID = process.env.PHONE_NUMBER_ID;
+class Config implements configType{
+    public webPort = process.env.PORT ? parseInt(process.env.PORT) : undefined;
+    public wppAPIToken = process.env.WHATSAPP_TOKEN;
+    public ownNumberID = process.env.PHONE_NUMBER_ID;
+    public baseUrl = "https://graph.facebook.com";
+};
+
+export default new Config();
