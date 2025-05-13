6adba6f9913627481055150045daf4f33a7ba54a
Fede_B <83786463+fedeb123@users.noreply.github.com>
2025-05-13 16:31:43 -0300

    added error exception when .env or config.js has a property with undefined value

--- server.ts
+++ server.ts
@@ -3,12 +3,21 @@ import dotenv from 'dotenv';
 import connectToMongo from './Infrastructure/database/mongo.js';
 import logger from './Infrastructure/logging/logger.js';
 import api from './API/index.js';
+import config from './config.js'
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 logger.info('Starting server...');
+
+const configPropiedades = Object.values(config);
+configPropiedades.forEach(propiedad => {
+    if (!propiedad){
+        logger.error('Archivo .env o config incompleto o no existente, abortando...');
+        throw new Error('Propiedad en archivo .env o config no definida');
+    }
+});
 
 await connectToMongo();
 
