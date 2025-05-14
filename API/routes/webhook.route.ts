a59e26ca37578e9010155d40849e544fa268eb13
Fede_B <83786463+fedeb123@users.noreply.github.com>
2025-05-14 18:27:57 -0300

    Added Webhook Processing

--- API/routes/webhook.route.ts
+++ API/routes/webhook.route.ts
@@ -1,20 +1,42 @@
 import { Router } from 'express';
+import config from '../../config.js';
 
 const router = Router();
 
-router.get('/api/webhook', (req, res) => {
-    /* handshake de subcripcion a eventos de mensaje. Se recibe: 
+interface urlQuery {
+    'hub.mode': string,
+    'hub.challenge': number,
+    'hub.verify_token': string
+}
+
+router.get('/api/webhook', function(req, res) {
+    /* handshake de subcripcion a eventos de mensaje. Se recibe como URL Query: 
     {    
         hub.mode=subscribe&
         hub.challenge=1158201444&
         hub.verify_token=meatyhamhock
     } */
+
+    const query = req.query as unknown as urlQuery;
+    const mode: string = query['hub.mode'];
+    const challenge: number = query['hub.challenge'];
+    const verifyToken: string = query['hub.verify_token'];
+
+    if (!challenge || !verifyToken || !mode){
+        res.status(400).json({ error: 'Parametros Incorrectos' });
+    }
     
-    console.log(req.query);
-    res.status(200).send('bruh');
+    if (verifyToken !== config.wppOwnWebhookToken){
+        res.status(401).json({ error: 'Token Incorrecto' });
+    }
+
+    res.status(200).send(challenge);            
+
 });
 
 
-//router.post('/api/webhook', );
+router.post('/api/webhook', function(req, res) {
+    console.log(req);
+});
 
 export default router;
