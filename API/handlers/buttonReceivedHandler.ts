31fc0774a37e4ad918f67d828f58467f840f5948
fedeb123 <federicoab@gmail.com>
2025-06-19 00:21:17 -0300

    readability

--- API/handlers/buttonReceivedHandler.ts
+++ API/handlers/buttonReceivedHandler.ts
@@ -27,19 +27,28 @@ export default async function handleIncomingButton(button: IButton, userPhoneNum
         case 'ADVICE_BUTTON':
             reply.message = 'Aguarde...';
             await sendUserReply(reply); 
+
             await askModelForAdvice(userPhoneNumber);
+
             userTemplateFlow.template = config.midFlowTemplateFlowName ?? "seguriamigo_user_error_flow";
-            await sendTemplate(userTemplateFlow);            
+            await sendTemplate(userTemplateFlow);      
+
             break;
+
         case 'SHARE_BUTTON':
             userTemplateFlow.template = config.shareFlowTemplateFlowName ?? "seguriamigo_user_error_flow";
             await sendTemplate(userTemplateFlow);
+
             userTemplateFlow.template = config.terminateFlowTemplateFlowName ?? "seguriamigo_user_error_flow";
             await sendTemplate(userTemplateFlow);
+
             break;
+
         case 'TERMINATE_BUTTON':
             userTemplateFlow.template = config.terminateFlowTemplateFlowName ?? "seguriamigo_user_error_flow";
+            
             await sendTemplate(userTemplateFlow);
+            
             break;
     
         default:
